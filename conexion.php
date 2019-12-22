6c1183164216962bf66f00a099603fbd9aa0ed97
Andrew Steeve Ramírez Guzmán <Aramirez@intergrupo.com>
2019-12-22 15:04:41 -0500

    Initial commit

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+try {
+    $base = new PDO('mysql:host=localhost; dbname=usuario', 'root', 'Andrewseigokan9');
+    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $base->exec("SET CHARACTER SET utf8");
+} catch (Exception $e) {
+    die('Error' . $e->getLine());
+    echo ("Linea del error" .  $e->getLine());
+}
+
+?>
